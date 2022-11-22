655ba754b705d1f7200e6d30c3920dfd2d72e035
João Khouri <joaovkhouri@gmail.com>
2022-11-22 14:38:59 -0300

    Initial commit

---
+++ src/pages/Movie.jsx
@@ -0,0 +1,82 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import {
+  BsGraphUp,
+  BsWallet2,
+  BsHourglassSplit,
+  BsFillFileEarmarkTextFill,
+} from "react-icons/bs";
+
+import MovieCard from "../components/MovieCard";
+
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
+import "./Movie.css";
+
+const Movie = () => {
+  const { id } = useParams();
+  const [movie, setMovie] = useState(null);
+
+  const getMovie = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    setMovie(data);
+  };
+
+  const formatCurrency = (number) => {
+    return number.toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD",
+    });
+  };
+
+  useEffect(() => {
+    const movieUrl = `${moviesURL}${id}?${apiKey}&language=pt-BR`;
+    getMovie(movieUrl);
+  });
+
+  return (
+    <div className="movie-page">
+      {movie && (
+        <>
+          <div className="movie-container">
+            <MovieCard movie={movie} showLink={false} />
+          </div>
+          <div className="description">
+            <div className="info">
+              <p className="tagline">"{movie.tagline}"</p>
+            </div>
+            <div className="info">
+              <h3>
+                <BsWallet2 /> Orçamento:
+              </h3>
+              <p>{formatCurrency(movie.budget)}</p>
+            </div>
+            <div className="info">
+              <h3>
+                <BsGraphUp /> Receita:
+              </h3>
+              <p>{formatCurrency(movie.revenue)}</p>
+            </div>
+            <div className="info">
+              <h3>
+                <BsHourglassSplit /> Duração:
+              </h3>
+              <p>{movie.runtime} minutos</p>
+            </div>
+            <div className="info">
+              <h3>
+                <BsFillFileEarmarkTextFill /> Descrição:
+              </h3>
+              <p>{movie.overview}</p>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Movie;
